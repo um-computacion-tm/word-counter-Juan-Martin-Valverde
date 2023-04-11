43be37ad606b2dd5e45a58430eef294b3cdb7168
Juan Martin Valaverde <juanmartinvalaverde@pop-os.localdomain>
2023-04-10 21:00:38 -0300

    pequeño avance solo tengo los test necesito consultar por bibliotecas

---
+++ contador_de_palabras.py
@@ -0,0 +1,18 @@
+import unittest 
+# P = input("escriba frase que desea contar: ")
+# String = P.split( )
+
+class TestCountWords(unittest.TestCase):
+    def test_una_palabra (self):
+        if P == String.value(1):
+            return (1)
+
+    def test_dos_palabras (self):
+        if P == String.value(2):
+            return (2)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
